0b04dfced746a2897e85fa540325c48460fefac0
deslrey <2135232170@qq.com>
2025-03-28 13:07:53 +0800

    feat: add react-blog-boot

---
+++ react-blog-boot/src/main/java/org/deslre/ReactBlogBootApplication.java
@@ -0,0 +1,13 @@
+package org.deslre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactBlogBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactBlogBootApplication.class, args);
+    }
+
+}
