ce850e22c9f2245e3964a53aec3e7123da24c878
deslrey <2135232170@qq.com>
2025-03-28 13:32:49 +0800

    feat: common sense tools

---
+++ commons/src/main/java/org/deslre/commons/utils/FinalUtils.java
@@ -0,0 +1,11 @@
+package org.deslre.commons.utils;
+
+/**
+ * ClassName: FinalUtils
+ * Description: 常量工具类
+ * Author: Deslrey
+ * Date: 2025-03-28 13:30
+ * Version: 1.0
+ */
+public class FinalUtils {
+}
