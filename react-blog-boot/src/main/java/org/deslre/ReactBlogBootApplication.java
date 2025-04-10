7510b9c5f039a8afb4dc016d8a2ee7020159a85c
deslrey <2135232170@qq.com>
2025-04-10 09:35:43 +0800

    feat: add MapperScan

--- react-blog-boot/src/main/java/org/deslre/ReactBlogBootApplication.java
+++ react-blog-boot/src/main/java/org/deslre/ReactBlogBootApplication.java
@@ -1,9 +1,11 @@
 package org.deslre;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("org.deslre.desk.mapper")
 public class ReactBlogBootApplication {
 
     public static void main(String[] args) {
