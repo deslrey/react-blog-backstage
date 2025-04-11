ad4200f2a0dc79e368353b3a3ca707456420dc83
deslrey <2135232170@qq.com>
2025-04-11 16:51:40 +0800

    feat: add ArticleConvert

---
+++ react-blog-boot/src/main/java/org/deslre/desk/convert/ArticleConvert.java
@@ -0,0 +1,22 @@
+package org.deslre.desk.convert;
+
+import org.deslre.desk.entity.po.Article;
+import org.deslre.desk.entity.vo.ArticleVO;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+/**
+ * ClassName: ArticleConvert
+ * Description: TODO
+ * Author: Deslrey
+ * Date: 2025-04-10 9:51
+ * Version: 1.0
+ */
+@Mapper(componentModel = "spring")
+public interface ArticleConvert {
+
+    ArticleConvert INSTANCE = Mappers.getMapper(ArticleConvert.class);
+
+    ArticleVO convertVO(Article article);
+
+}
