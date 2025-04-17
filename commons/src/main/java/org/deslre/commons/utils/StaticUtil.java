b69a8f3eda2dd0047da3554cd697dd87b0b12ca7
deslrey <2135232170@qq.com>
2025-04-17 21:09:51 +0800

    feat: add StaticUtil

---
+++ commons/src/main/java/org/deslre/commons/utils/StaticUtil.java
@@ -0,0 +1,15 @@
+package org.deslre.commons.utils;
+
+/**
+ * ClassName: StaticUtil
+ * Description: 状态工具类
+ * Author: Deslrey
+ * Date: 2025-04-16 11:17
+ * Version: 1.0
+ */
+public class StaticUtil {
+
+    public static final Boolean TRUE = true;
+    public static final Boolean FALSE = false;
+
+}
