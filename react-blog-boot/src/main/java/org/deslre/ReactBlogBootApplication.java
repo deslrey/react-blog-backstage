bc420bca6cf2d081c5adc9ff144e1ce4cbcde1c7
deslrey <2135232170@qq.com>
2025-04-18 15:38:50 +0800

    feat: add multiple package scans

--- react-blog-boot/src/main/java/org/deslre/ReactBlogBootApplication.java
+++ react-blog-boot/src/main/java/org/deslre/ReactBlogBootApplication.java
@@ -1,11 +1,12 @@
 package org.deslre;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.mybatis.spring.annotation.MapperScans;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("org.deslre.desk.mapper")
+@MapperScan({"org.deslre.desk.mapper","org.deslre.user.mapper"})
 public class ReactBlogBootApplication {
 
     public static void main(String[] args) {
