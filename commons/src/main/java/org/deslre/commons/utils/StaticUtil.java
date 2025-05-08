85a9d61c8965adeb9594cbb687a1c06f0eae68e1
deslrey <2135232170@qq.com>
2025-05-08 20:27:04 +0800

    feat: add RESOURCE_URL

--- commons/src/main/java/org/deslre/commons/utils/StaticUtil.java
+++ commons/src/main/java/org/deslre/commons/utils/StaticUtil.java
@@ -11,5 +11,7 @@ public class StaticUtil {
 
     public static final Boolean TRUE = true;
     public static final Boolean FALSE = false;
-
+    public static final String RESOURCE_URL = "http://localhost:8080/deslre/staticSource/";
+    public static final String RESOURCE_URL_IMAGE = "http://localhost:8080/deslre/staticSource/image/";
+    public static final String RESOURCE_URL_MD = "http://localhost:8080/deslre/staticSource/md/";
 }
