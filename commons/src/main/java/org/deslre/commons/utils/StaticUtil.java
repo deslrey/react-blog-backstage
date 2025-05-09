632253fb9910281ff813de6941465de1ff3793d6
deslrey <2135232170@qq.com>
2025-05-09 16:51:46 +0800

    docs: add comment

--- commons/src/main/java/org/deslre/commons/utils/StaticUtil.java
+++ commons/src/main/java/org/deslre/commons/utils/StaticUtil.java
@@ -11,8 +11,11 @@ public class StaticUtil {
 
     public static final Boolean TRUE = true;
     public static final Boolean FALSE = false;
+    //    静态资源路径
     public static final String RESOURCE_URL = "http://localhost:8080/deslre/staticSource/";
+    //    图片url
     public static final String RESOURCE_URL_IMAGE = "http://localhost:8080/deslre/staticSource/image/";
+    //    md文件的url
     public static final String RESOURCE_URL_MD = "http://localhost:8080/deslre/staticSource/md/";
     //  默认方面
     public static final String DEFAULT_COVER = RESOURCE_URL + "cover.webp";
