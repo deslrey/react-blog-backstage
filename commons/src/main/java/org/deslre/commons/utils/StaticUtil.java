1d99b678f7c42ca3ce1ca99553285261ed9b0dd6
deslrey <2135232170@qq.com>
2025-05-10 19:55:05 +0800

    style：format

--- commons/src/main/java/org/deslre/commons/utils/StaticUtil.java
+++ commons/src/main/java/org/deslre/commons/utils/StaticUtil.java
@@ -12,7 +12,7 @@ public class StaticUtil {
     public static final Boolean TRUE = true;
     public static final Boolean FALSE = false;
 
-    //  文件映射路径
+    //    文件映射路径
     public static final String FILE_PATH = "E:\\staticSource\\";
 
     //    静态资源路径
@@ -28,16 +28,15 @@ public class StaticUtil {
     public static final String RESOURCE_URL_MD = "http://localhost:8080/deslre/staticSource/md/";
     //    md文件存储路径
     public static final String RESOURCE_MD = FILE_PATH + "md\\";
-    //  默认封面
+    //    默认封面
     public static final String DEFAULT_COVER = RESOURCE_DEFAULT + "cover.webp";
-    //默认头像
+    //    默认头像
     public static final String DEFAULT_AVATAR = RESOURCE_URL + "avatar.jpg";
-    //  默认标题
+    //    默认标题
     public static final String DEFAULT_TITLE = "Deslrey的博客";
-    //  默认作者
+    //    默认作者
     public static final String DEFAULT_AUTHOR = "Deslrey";
-    //  默认描述
+    //    默认描述
     public static final String DEFAULT_DESCRIPTION = "Deslrey的个人博客，记录生活中的点滴，分享编程的乐趣。";
-
 
 }
