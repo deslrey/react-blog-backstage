576e00c2c0ac164cea2acdcd3e4b198401539c8e
deslrey <2135232170@qq.com>
2025-05-10 19:08:28 +0800

    feat: add static data

--- commons/src/main/java/org/deslre/commons/utils/StaticUtil.java
+++ commons/src/main/java/org/deslre/commons/utils/StaticUtil.java
@@ -13,10 +13,22 @@ public class StaticUtil {
     public static final Boolean FALSE = false;
     //    静态资源路径
     public static final String RESOURCE_URL = "http://localhost:8080/deslre/staticSource/";
+    //    静态资源路径
+    public static final String RESOURCE_DEFAULT = "http://localhost:8080/deslre/staticSource/default/";
     //    图片url
     public static final String RESOURCE_URL_IMAGE = "http://localhost:8080/deslre/staticSource/image/";
     //    md文件的url
     public static final String RESOURCE_URL_MD = "http://localhost:8080/deslre/staticSource/md/";
-    //  默认方面
-    public static final String DEFAULT_COVER = RESOURCE_URL + "cover.webp";
+    //  默认封面
+    public static final String DEFAULT_COVER = RESOURCE_DEFAULT + "cover.webp";
+    //默认头像
+    public static final String DEFAULT_AVATAR = RESOURCE_URL + "avatar.jpg";
+    //  默认标题
+    public static final String DEFAULT_TITLE = "Deslrey的博客";
+    //  默认作者
+    public static final String DEFAULT_AUTHOR = "Deslrey";
+    //  默认描述
+    public static final String DEFAULT_DESCRIPTION = "Deslrey的个人博客，记录生活中的点滴，分享编程的乐趣。";
+
+
 }
