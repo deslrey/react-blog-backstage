8e73a0ba02d1ed270d79ad91987fabe698172acc
deslrey <2135232170@qq.com>
2025-06-02 17:38:15 +0800

    fix: redis util

--- react-blog-boot/src/main/java/org/deslre/utils/RedisUtil.java
+++ react-blog-boot/src/main/java/org/deslre/utils/RedisUtil.java
@@ -1,9 +1,17 @@
 package org.deslre.utils;
 
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.bean.copier.CopyOptions;
+import cn.hutool.json.JSONUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.connection.DataType;
+import org.springframework.data.redis.core.ListOperations;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
 
 /**
  * ClassName: RedisUtil
@@ -12,10 +20,235 @@ import javax.annotation.Resource;
  * Date: 2025-05-30 16:59
  * Version: 1.0
  */
+@Slf4j
 @Component
 public class RedisUtil {
 
     @Resource
     private StringRedisTemplate stringRedisTemplate;
+
+    /**
+     * 删除key
+     */
+    public void delete(String key) {
+        stringRedisTemplate.delete(key);
+    }
+
+    /**
+     * 批量删除key
+     */
+    public void delete(Collection<String> keys) {
+        stringRedisTemplate.delete(keys);
+    }
+
+    /**
+     * 序列化key
+     */
+    public byte[] dump(String key) {
+        return stringRedisTemplate.dump(key);
+    }
+
+    /**
+     * 是否存在key
+     */
+    public Boolean hasKey(String key) {
+        return stringRedisTemplate.hasKey(key);
+    }
+
+    /**
+     * 设置过期时间
+     */
+    public Boolean expire(String key, long timeout, TimeUnit unit) {
+        return stringRedisTemplate.expire(key, timeout, unit);
+    }
+
+    /**
+     * 设置过期时间
+     */
+    public Boolean expireAt(String key, Date date) {
+        return stringRedisTemplate.expireAt(key, date);
+    }
+
+    /**
+     * 查找匹配的key
+     */
+    public Set<String> keys(String pattern) {
+        return stringRedisTemplate.keys(pattern);
+    }
+
+    /**
+     * 将当前数据库的 key 移动到给定的数据库 db 当中
+     */
+    public Boolean move(String key, int dbIndex) {
+        return stringRedisTemplate.move(key, dbIndex);
+    }
+
+    /**
+     * 移除 key 的过期时间，key 将持久保持
+     */
+    public Boolean persist(String key) {
+        return stringRedisTemplate.persist(key);
+    }
+
+    /**
+     * 返回 key 的剩余的过期时间
+     */
+    public Long getExpire(String key, TimeUnit unit) {
+        return stringRedisTemplate.getExpire(key, unit);
+    }
+
+    /**
+     * 返回 key 的剩余的过期时间
+     */
+    public Long getExpire(String key) {
+        return stringRedisTemplate.getExpire(key);
+    }
+
+    /**
+     * 从当前数据库中随机返回一个 key
+     */
+    public String randomKey() {
+        return stringRedisTemplate.randomKey();
+    }
+
+    /**
+     * 修改 key 的名称
+     */
+    public void rename(String oldKey, String newKey) {
+        stringRedisTemplate.rename(oldKey, newKey);
+    }
+
+    /**
+     * 仅当 newkey 不存在时，将 oldKey 改名为 newkey
+     */
+    public Boolean renameIfAbsent(String oldKey, String newKey) {
+        return stringRedisTemplate.renameIfAbsent(oldKey, newKey);
+    }
+
+    /**
+     * 返回 key 所储存的值的类型
+     */
+    public DataType type(String key) {
+        return stringRedisTemplate.type(key);
+    }
+
+    /* -------------------string相关操作--------------------- */
+
+    /**
+     * 设置指定 key 的值
+     */
+    public void set(String key, String value) {
+        stringRedisTemplate.opsForValue().set(key, value);
+    }
+
+    /**
+     * 获取指定 key 的值
+     */
+    public String get(String key) {
+        return stringRedisTemplate.opsForValue().get(key);
+    }
+
+    /**
+     * 返回 key 中字符串值的子字符
+     */
+    public String getRange(String key, long start, long end) {
+        return stringRedisTemplate.opsForValue().get(key, start, end);
+    }
+
+    /**
+     * 将给定 key 的值设为 value ，并返回 key 的旧值(old value)
+     */
+    public String getAndSet(String key, String value) {
+        return stringRedisTemplate.opsForValue().getAndSet(key, value);
+    }
+
+    /**
+     * 对 key 所储存的字符串值，获取指定偏移量上的位(bit)
+     */
+    public Boolean getBit(String key, long offset) {
+        return stringRedisTemplate.opsForValue().getBit(key, offset);
+    }
+
+    /**
+     * 批量获取
+     */
+    public List<String> multiGet(Collection<String> keys) {
+        return stringRedisTemplate.opsForValue().multiGet(keys);
+    }
+
+    /**
+     * 设置ASCII码, 字符串'a'的ASCII码是97, 转为二进制是'01100001', 此方法是将二进制第offset位值变为value
+     *
+     * @param value 值,true为1, false为0
+     */
+    public boolean setBit(String key, long offset, boolean value) {
+        return Boolean.TRUE.equals(stringRedisTemplate.opsForValue().setBit(key, offset, value));
+    }
+
+    /**
+     * 将值 value 关联到 key ，并将 key 的过期时间设为 timeout
+     *
+     * @param timeout 过期时间
+     * @param unit    时间单位, 天:TimeUnit.DAYS 小时:TimeUnit.HOURS 分钟:TimeUnit.MINUTES
+     *                秒:TimeUnit.SECONDS 毫秒:TimeUnit.MILLISECONDS
+     */
+    public void setEx(String key, String value, long timeout, TimeUnit unit) {
+        stringRedisTemplate.opsForValue().set(key, value, timeout, unit);
+    }
+
+    /**
+     * 只有在 key 不存在时设置 key 的值
+     *
+     * @return 之前已经存在返回false, 不存在返回true
+     */
+    public boolean setIfAbsent(String key, String value) {
+        return Boolean.TRUE.equals(stringRedisTemplate.opsForValue().setIfAbsent(key, value));
+    }
+
+    /**
+     * 用 value 参数覆写给定 key 所储存的字符串值，从偏移量 offset 开始
+     *
+     * @param offset 从指定位置开始覆写
+     */
+    public void setRange(String key, String value, long offset) {
+        stringRedisTemplate.opsForValue().set(key, value, offset);
+    }
+
+    /**
+     * 获取字符串的长度
+     */
+    public Long size(String key) {
+        return stringRedisTemplate.opsForValue().size(key);
+    }
+
+    /**
+     * 批量添加
+     */
+    public void multiSet(Map<String, String> maps) {
+        stringRedisTemplate.opsForValue().multiSet(maps);
+    }
+
+    /**
+     * 同时设置一个或多个 key-value 对，当且仅当所有给定 key 都不存在
+     *
+     * @return 之前已经存在返回false, 不存在返回true
+     */
+    public boolean multiSetIfAbsent(Map<String, String> maps) {
+        return Boolean.TRUE.equals(stringRedisTemplate.opsForValue().multiSetIfAbsent(maps));
+    }
+
+    /**
+     * 增加(自增长), 负数则为自减
+     */
+    public Long incrBy(String key, long increment) {
+        return stringRedisTemplate.opsForValue().increment(key, increment);
+    }
+
+    /**
+     * 追加到末尾
+     */
+    public Integer append(String key, String value) {
+        return stringRedisTemplate.opsForValue().append(key, value);
+    }
 
 }
