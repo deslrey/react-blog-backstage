b00a721c4520f9aaabd6c52625ca8a04a9225bbf
deslrey <2135232170@qq.com>
2025-06-03 10:30:23 +0800

    feat: add vo

---
+++ react-blog-boot/src/main/java/org/deslre/user/entity/vo/VisitLogVO.java
@@ -0,0 +1,31 @@
+package org.deslre.user.entity.vo;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import lombok.Data;
+
+import java.io.Serial;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+/**
+ * ClassName: VisitLogVO
+ * Description: 访问日志VO
+ * Author: Deslrey
+ * Date: 2025-06-03 10:27
+ * Version: 1.0
+ */
+@Data
+public class VisitLogVO {
+
+    private Long id;
+
+    private String visitorIp;
+
+    private Long articleId;
+
+    private LocalDateTime visitTime;
+
+    private LocalDate visitDate;
+
+}
---
+++ react-blog-boot/src/main/java/org/deslre/user/entity/vo/VisitorVO.java
@@ -0,0 +1,41 @@
+package org.deslre.user.entity.vo;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import lombok.Data;
+
+import java.io.Serial;
+import java.time.LocalDateTime;
+
+/**
+ * ClassName: VisitorVO
+ * Description: 访客信息VO
+ * Author: Deslrey
+ * Date: 2025-06-03 10:24
+ * Version: 1.0
+ */
+@Data
+public class VisitorVO {
+
+    private Long id;
+
+    private String ip;
+
+    private String platform;
+
+    private String browser;
+
+    private String device;
+
+    private String province;
+
+    private String city;
+
+    private Integer visitCount;
+
+    private LocalDateTime firstVisit;
+
+    private LocalDateTime lastVisit;
+
+    private String visitorToken;
+}
