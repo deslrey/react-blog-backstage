1480c7ef8125c9b49c47e4fb0474df2593740069
deslrey <2135232170@qq.com>
2025-06-03 11:54:27 +0800

    feat: unified return

---
+++ react-blog-boot/src/main/java/org/deslre/user/entity/po/Region.java
@@ -0,0 +1,27 @@
+package org.deslre.user.entity.po;
+
+import lombok.Data;
+
+/**
+ * ClassName: Region
+ * Description: 地区
+ * Author: Deslrey
+ * Date: 2025-06-03 11:48
+ * Version: 1.0
+ */
+@Data
+public class Region {
+    /**
+     * 国家
+     */
+    private String country;
+    /**
+     * 城市
+     */
+    private String city;
+    /**
+     * 注册地
+     */
+    private String regionName;
+
+}
