87d3085c9167d4fe238ef91a4aca99b69b6bea8a
deslrey <2135232170@qq.com>
2025-06-08 20:26:43 +0800

    feat: MetadataDTO

---
+++ react-blog-boot/src/main/java/org/deslre/desk/entity/dto/MetadataDTO.java
@@ -0,0 +1,18 @@
+package org.deslre.desk.entity.dto;
+
+import lombok.Data;
+
+/**
+ * ClassName: MetadataDTO
+ * Description: SEO
+ * Author: Deslrey
+ * Date: 2025-06-08 9:59
+ * Version: 1.0
+ */
+@Data
+public class MetadataDTO {
+
+    private String title;
+
+    private String description;
+}
